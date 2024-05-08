db124717114f8d83b139f815bc26773c0844d8b8
poodinh <goncalo.cardoso098@gmail.com>
2024-05-08 22:53:34 +0100

    aula 8/5

---
+++ teste/script.js
@@ -0,0 +1,12 @@
+let v1= 1;
+let v2= 2;
+let v3= false;
+let v4= null;
+let v5= undefined;
+let v6={
+    name: 'Gonçalo',
+    lastname:'Cardoso'
+};
+console.log(v1);
+let testeOperadores= !(v1==v2) && v6;
+console.log(testeOperadores);

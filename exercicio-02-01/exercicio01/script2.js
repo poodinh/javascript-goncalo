04353a24054e4347370c640374012ced75aadb8b
poodinh <goncalo.cardoso098@gmail.com>
2024-05-15 19:12:10 +0100

    .

--- exercicio-02-01/exercicio01/script2.js
+++ exercicio-02-01/exercicio01/script2.js
@@ -1,10 +1,10 @@
 
 function desconto(price, discount){
 if(discount>0 && discount<10) {
-    return finalPrice=price+((100-discount)/100)*price;
+    return finalPrice=price+(discount/100)*price;
 }
 else if(discount>=10 && discount<30) {
-    return finalPrice=price-((100-discount)/100)*price;
+    return finalPrice=price-(discount/100)*price;
 }
 else if(discount===0 || discount===undefined) {
     return finalPrice=2*price;
